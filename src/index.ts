80bf06405418ed737c68ac3010dc61df98a41c4b
Rutger <34132104+undefined@users.noreply.github.com>
2022-03-17 10:22:11 +0000

    First commit

---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import { html, css, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { sendMessage, onMessagesUpdated } from './room-connection';
+
+
+// Check out README.md for the instructions
+
+// TODO: Your new component(s) go here! You can render them from the App component
+
+
+
+@customElement('custom-chat')
+class CustomChat extends LitElement { 
+  handleclick(){
+    console.log('werkt het?'))
+  }
+  render() {
+    return html`
+      <input type="text" placeholder="username">
+      <input type="text" placeholder="message">
+      <button @click=${this.handleClick}>Send</button>
+    `;
+  }
+}
+
+
+@customElement('lit-app')
+class App extends LitElement {
+  @property({type: String})
+  title = 'Realtime Chat';
+
+
+
+  static styles = css`
+    :host {
+      font-family: sans-serif;
+      display: block;
+      padding: 20px;
+    }
+
+    h1 {
+      color: #00a667;
+    }
+  `;
+
+  render() {
+    return html`
+      <h1>${this.title}</h1>
+      <custom-chat>${this.} </custom-chat>
+    `;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+
+
+
+  👀 Looking for an internship or job at the best employer in the Netherlands? 👀
+                        👉 https://arcady.nl/ 👈
+
+
+
+*/
